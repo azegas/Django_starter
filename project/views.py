6476b6e847a20478dc68a6be870b48d37bdbf338
lionginas <azegaspa@gmail.com>
2024-05-30 15:38:24 +0300

    trying to trigger black

--- project/views.py
+++ project/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 
+
 class Index(View):
     def get(self, request):
         return render(request, "project/index.html")
 
+    
 def test(request):
     return HttpResponse('<h2>Test</h2>')
