992c996d749e599d64511a91644c3ec3f8164666
lionginas <azegaspa@gmail.com>
2024-05-30 16:50:53 +0300

    change admin link

--- project/urls.py
+++ project/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 from project.views import Index, test
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("administratorius/", admin.site.urls),
     path("accounts/", include("apps.accounts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", Index.as_view(), name="index"),
