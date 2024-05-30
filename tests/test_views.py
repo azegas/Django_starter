db600a4424710438128fe0fe9f969ceea0888905
lionginas <azegaspa@gmail.com>
2024-05-30 14:29:29 +0300

    add first test

---
+++ tests/test_views.py
@@ -0,0 +1,24 @@
+from django.test import Client, TestCase
+from django.urls import reverse
+
+
+class TestViews(TestCase):
+    """Class for view tests"""
+
+    def setUp(self):
+        """
+        setUp method is simply for creating all kinds of objects that we will
+        use/reuse in the tests below, later.
+        """
+
+        self.client = Client()
+        self.index_url = reverse("index")
+
+    def test_index_get(self):
+        """test index view"""
+
+        response = self.client.get(self.index_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "base.html")
+        self.assertTemplateUsed(response, "project/index.html")
