db95877375c8b29e1eb0c68eff6b00e55513b2db
nixos <azegaspa@gmail.com>
2024-06-06 16:43:03 +0300

    use sqlite db by default

--- project/settings.py
+++ project/settings.py
@@ -93,10 +93,18 @@ WSGI_APPLICATION = "project.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # },
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRESQL_LOCAL_DB_NAME"),
+        "USER": os.getenv("POSTGRESQL_LOCAL_DB_USER"),
+        "PASSWORD": os.getenv("POSTGRESQL_LOCAL_DB_PASSWORD"),
+        "HOST": os.getenv("POSTGRESQL_LOCAL_DB_HOST"),
+        "PORT": os.getenv("POSTGRESQL_LOCAL_DB_PORT"),
+    },
 }
 
 
